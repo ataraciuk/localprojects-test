4d2345393fbee6d84a4ab2821105ad4ffb631076
ataraciuk <ataraciuk@gmail.com>
2014-05-30 16:03:26 -0400

    debugging database string

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import os
 from flask import Flask
+import os
+import datetime
+import pymongo
+from pymongo import MongoClient
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+	return MONGO_URL or 'no env'
+
+if __name__ == '__main__':
+    app.run(debug=True)
