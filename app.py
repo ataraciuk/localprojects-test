55fb02b9a5148f0db42701b5c76dd6f5aef9a343
ataraciuk <ataraciuk@gmail.com>
2014-05-31 20:07:47 -0400

    rounds editing and deleting done

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def statistics():
 
 @app.route('/admin/rounds/')
 def rounds():
-	statistics = db.statistics.find()
+	statistics = list(db.statistics.find())
 	elems = db.rounds.find()
 	return render_template('rounds.html', elems=elems, statistics=statistics)
 
@@ -50,17 +50,34 @@ def updateStat(stat_id):
 	db.statistics.save({'name':name, '_id': ObjectId(stat_id)})
 	return ''
 
+@app.route('/admin/rounds/<round_id>/update/', methods=['POST'])
+def updateRound(round_id):
+	data = {'inGame' : request.form.has_key('inGame'), '_id': ObjectId(round_id), 'question': request.form['question'], 'yes': {}, 'no': {}}
+	for key, val in request.form.iteritems():
+		keys = key.split('-')
+		if (len(keys) > 1):
+			data[keys[0]][keys[1]] = int(val)
+	db.rounds.save(data)
+	return ''
+
 @app.route('/admin/statistics/<stat_id>/delete/', methods=['POST'])
 def deleteStat(stat_id):
 	db.statistics.remove({'_id': ObjectId(stat_id)})
 	return ''
 
+@app.route('/admin/rounds/<round_id>/delete/', methods=['POST'])
+def deleteRound(round_id):
+	db.rounds.remove({'_id': ObjectId(round_id)})
+	return ''
+
 @app.route('/admin/rounds/new/', methods=['POST'])
 def newRound():
 	name = request.form['question']
-	new_id = db.rounds.insert({'question': name, 'inGame': True})
-	statistics = db.statistics.find()
-	return render_template('round.html', elem={'question': name, '_id': new_id}, statistics=statistics)
+	data = {'question': name, 'inGame': True, 'yes': {}, 'no': {}}
+	new_id = db.rounds.insert(data)
+	data['_id'] = new_id
+	statistics = list(db.statistics.find())
+	return render_template('round.html', elem=data, statistics=statistics)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/rounds.js
+++ static/js/rounds.js
@@ -13,10 +13,37 @@ rounds.init = function() {
 			});
 		}
 	});
+	$('body').on('click', '.update-btn', function(){
+		var btn = $(this);
+		var form = btn.prev().prev();
+		var newSt = form.find('input[name="question"]').val();
+		if(newSt.length > 0) {
+			form.find('input[type="number"]').each(function(){
+				var elem = $(this);
+				var value = parseInt(elem.val(), 10);
+				value = isNaN(value) ? 0 : value;
+				value = value < -5 ? -5 : value;
+				value = value > 5 ? 5 : value;
+				elem.val(value);
+			});
+			var status = btn.next().next().stop().html('Updating...').show().css('opacity','1');
+			$.post('/admin/rounds/'+btn.prev().val()+'/update/', form.serialize(), function(){
+				status.html('Updated!').fadeOut(4000);
+			});
+		}
+	});
+	$('body').on('click', '.delete-btn', function(){
+		var btn = $(this);
+		if (window.confirm("Confirm deletion")) { 
+			$.post('/admin/rounds/'+btn.prev().prev().val()+'/delete/', function(){
+				btn.parent().fadeOut(400, function(){$(this).remove();});
+			});  			
+		}
+	});
 }
 
 rounds.DOM = {
-	list: $('.statistics')
+	list: $('.rounds')
 }
 
 $(function(){
